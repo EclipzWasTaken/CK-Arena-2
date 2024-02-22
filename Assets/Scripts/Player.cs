b42525eb5b9b410dae4642b9929b144a55b75bcd
EclipzWasTaken <143146713+EclipzWasTaken@users.noreply.github.com>
2024-02-22 10:18:56 -0500

    Tried to add dashing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,29 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] TrailRenderer dashTrail;
+    
 
+    // Dashing Fields
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashSpeed = 200f;
+    private float dashTime = 0.1f;
+    private float dashCooldown = 1f;
+
+    // Movement Fields
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 12f;
     private bool isFacingRight = true;
+    private int numberOfJumps = 2;
 
     void Update()
     {
@@ -32,14 +44,25 @@ public class Player : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && IsGrounded())
+        if (context.performed && IsGrounded() && numberOfJumps > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            numberOfJumps--;
+        }
+        else if (context.performed && !IsGrounded() && numberOfJumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            numberOfJumps--;
         }
 
         if (context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (IsGrounded())
+        {
+            numberOfJumps = 1;
         }
     }
 
@@ -59,5 +82,36 @@ public class Player : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+
+
+    // Dash Input Action
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && canDash && !isDashing)
+        {
+            StartCoroutine(PerformDash());
+        }
+    }
+
+    /**
+        * Coroutine to perform the dash
+        The actual code for the dash is in this coroutine
+        */
+        
+    private IEnumerator PerformDash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0f);
+        dashTrail.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        dashTrail.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
